a4e6e14414842c754c80b09e38d48a9c8c190203
Nadav <nadavp122@gmail.com>
2020-10-28 17:30:57 +0200

    fix add-user endpoint

--- server/src/routes/users/index.ts
+++ server/src/routes/users/index.ts
@@ -5,25 +5,23 @@ import { db } from "../../db";
 const usersRouter = Router();
 
 usersRouter.post("/add-user", async (req: any, res) => {
-  const isUserExists = await db
-    .collection("users")
-    .findOne({ email: req.user.email });
+  let user = await db.collection("users").findOne({ email: req.user.email });
 
-  if (!isUserExists) {
-    const user = new UserModel({
+  if (!user) {
+    const newUser = new UserModel({
       email: req.user.email,
       displayName: req.user.name,
       uid: req.user.uid,
       picture: req.user.picture
     });
 
-    await db.collection("users").insertOne(user);
+    user = (await db.collection("users").insertOne(newUser)).ops;
   }
 
   const sanitizedUser = {
-    email: req.user.email,
-    displayName: req.user.name,
-    picture: req.user.picture
+    email: user.email,
+    displayName: user.displayName,
+    picture: user.picture
   };
 
   res.json(sanitizedUser);
